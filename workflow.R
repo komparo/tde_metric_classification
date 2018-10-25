72c7b35c79146e86480d3afe64dedbf324d3e2fb
Wouter Saelens <wouter.saelens@gmail.com>
2018-10-25 16:34:00 +0200

    fix for new certigo

--- workflow.R
+++ workflow.R
@@ -13,21 +13,19 @@ metric_design_all <- tibble(
 
 generate_metric_calls <- function(methods, workflow_folder = ".", scores_folder = "scores", metric_design = metric_design_all) {
   design <- crossing(
-    methods$design %>% rename(method_id = id) %>% bind_cols(methods$outputs) %>% select(-parameters),
+    methods$design %>% rename(method_id = id) %>% select(dataset_id, method_id, expression, tde_overall),
     metric_design
+  ) %>% 
+    mutate(
+      script = list(script_file(str_glue("{workflow_folder}/scripts/run.R"))),
+      executor = list(docker_executor("komparo/tde_method_random")),
+      accuracy = str_glue("{scores_folder}/{id}/{dataset_id}/{method_id}/accuracy.json") %>% purrr::map(derived_file)
   )
   
   rscript_call(
     "komparo/dummy",
     design = design,
-    inputs = design %>% 
-      select(expression, tde_overall, parameters) %>% 
-      mutate(
-        script = list(script_file(str_glue("{workflow_folder}/scripts/run.R"))),
-        executor = list(docker_executor("komparo/tde_method_random"))
-      ),
-    outputs = design %>% transmute(
-      accuracy = str_glue("{scores_folder}/{id}/{dataset_id}/{method_id}/accuracy.json") %>% purrr::map(derived_file)
-    )
+    inputs = c("script", "executor", "expression", "tde_overall", "parameters"),
+    outputs = c("accuracy")
   )
 }
