63557484d16998146f615c8c7d4474f3f9533fa4
Wouter Saelens <wouter.saelens@gmail.com>
2018-10-26 17:23:41 +0200

    fix with new certigo subworkflows

--- test.R
+++ test.R
@@ -1,41 +1,31 @@
-pull_or_clone <- function(repo, local_path) {
-  if (fs::dir_exists(local_path)) {
-    git2r::pull(local_path)
-  } else {
-    git2r::clone(repo, local_path = local_path)
-  }
-}
+library(certigo)
 
-pull_or_clone("https://github.com/komparo/tde_dataset_dyntoy", local_path = "modules/dataset")
-pull_or_clone("https://github.com/komparo/tde_method_random", local_path = "modules/method")
-
-source("modules/dataset/workflow.R")
-datasets <- generate_dataset_calls(
-  workflow_folder = "modules/dataset",
-  datasets_folder = "data/datasets",
-  dataset_design = dataset_design_all[1, ]
+datasets <- load_call_git(
+  "https://github.com/komparo/tde_dataset_dyntoy",
+  "modules/dataset",
+  derived_file_directory = "results/datasets"
 )
+datasets$design <- datasets$design[1, ]
 
-source("modules/method/workflow.R")
-methods <- generate_method_calls(
-  datasets,
-  workflow_folder = "modules/method",
-  method_design = method_design_all[1, ],
-  models_folder = "data/models"
+models <- load_call_git(
+  "https://github.com/komparo/tde_method_random",
+  "modules/method",
+  derived_file_directory = "results/models",
+  datasets = datasets
 )
+models$design <- models$design[1, ]
 
-source('workflow.R')
-metrics <- generate_metric_calls(
-  methods,
-  workflow_folder = ".",
-  scores_folder = "data/scores",
-  metric_design = metric_design_all[1, ]
+scores <- load_call(
+  "workflow.R", 
+  derived_file_directory = "results/scores", 
+  models = models
 )
+scores$design <- scores$design[1, ]
 
 workflow <- workflow(
   datasets,
-  methods,
-  metrics
+  models,
+  scores
 )
 workflow$reset()
 workflow$run()
--- workflow.R
+++ workflow.R
@@ -5,21 +5,21 @@ library(tidyr)
 library(stringr)
 library(dplyr)
 
-metric_design_all <- tibble(
-  average_accuracy = c(0, 0.5, 1)
-) %>% 
-  mutate(parameters = dynutils::mapdf(., parameters)) %>% 
-  mutate(id = as.character(seq_len(n())))
-
-generate_metric_calls <- function(methods, workflow_folder = ".", scores_folder = "scores", metric_design = metric_design_all) {
+get_call <- function(models) {
+  metric_design <- tibble(
+    average_accuracy = c(0, 0.5, 1)
+  ) %>% 
+    mutate(parameters = dynutils::mapdf(., parameters)) %>% 
+    mutate(id = as.character(seq_len(n())))
+  
   design <- crossing(
-    methods$design %>% rename(method_id = id) %>% select(dataset_id, method_id, expression, tde_overall),
+    models$design %>% rename(method_id = id) %>% select(dataset_id, method_id, expression, tde_overall),
     metric_design
   ) %>% 
     mutate(
-      script = list(script_file(str_glue("{workflow_folder}/scripts/run.R"))),
+      script = list(script_file(str_glue("scripts/run.R"))),
       executor = list(docker_executor("komparo/tde_method_random")),
-      accuracy = str_glue("{scores_folder}/{id}/{dataset_id}/{method_id}/accuracy.json") %>% purrr::map(derived_file)
+      accuracy = str_glue("{id}/{dataset_id}/{method_id}/accuracy.json") %>% purrr::map(derived_file)
   )
   
   rscript_call(
