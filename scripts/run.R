c3974c7705abd6d5b4eeeca42ad410996bca9164
Wouter Saelens <wouter.saelens@gmail.com>
2018-10-29 13:19:05 +0100

    add a lot more scores :)

--- scripts/run.R
+++ scripts/run.R
@@ -6,13 +6,30 @@ parameters <- jsonlite::fromJSON(inputs[["parameters"]])
 set.seed(parameters$seed)
 
 # read expression
-expression <- read.csv(inputs[["expression"]], row.names = 1) %>% as.matrix()
+expression <- read.csv(inputs[["dataset_expression"]], row.names = 1) %>% as.matrix()
 
 # read tde overall
-tde_overall <- readr::read_csv(inputs[["tde_overall"]])
+dataset_tde_overall <- readr::read_csv(inputs[["dataset_tde_overall"]])
+model_tde_overall <- readr::read_csv(inputs[["model_tde_overall"]])
 
-# score
-accuracy = rnorm(1, parameters[["average_accuracy"]], sd = 0.1)
+# calculate accuracy
+scores <- full_join(
+  dataset_tde_overall %>% rename(tde_overall_1 = tde_overall),
+  model_tde_overall %>% rename(tde_overall_2 = tde_overall)
+) %>% 
+  summarise(
+    n = n(),
+    TP = sum((tde_overall_1 == tde_overall_2) & tde_overall_1),
+    TN = sum((tde_overall_1 == tde_overall_2) & !tde_overall_1),
+    FP = sum((tde_overall_1 == !tde_overall_2) & !tde_overall_1),
+    FN = sum((tde_overall_1 == !tde_overall_2) & tde_overall_1),
+    P = sum(tde_overall_1),
+    N = sum(!tde_overall_1),
+    accuracy = (TP + TN) / n,
+    balanced_accuracy = (TP / P + TN / N) /2,
+    F1 = 2 * TP / (2 * TP + FP + FN)
+  ) %>% 
+  as.list()
 
 # write dataset
-jsonlite::write_json(accuracy, outputs[["accuracy"]])
+jsonlite::write_json(scores, outputs[["accuracy"]])
--- workflow.R
+++ workflow.R
@@ -5,18 +5,24 @@ library(tidyr)
 library(stringr)
 library(dplyr)
 
+models$design %>% dynutils::mapdf(identity)
+
 get_call <- function(models) {
   metric_design <- tibble(
     average_accuracy = c(0, 0.5, 1)
   ) %>% 
-    mutate(parameters = dynutils::mapdf(., parameters)) %>% 
-    mutate(id = as.character(seq_len(n())))
+    mutate(
+      parameters = dynutils::mapdf(., parameters),
+      id = as.character(seq_len(n()))
+    )
   
   design <- crossing(
-    models$design %>% rename(method_id = id) %>% select(dataset_id, method_id, expression, tde_overall),
+    tibble(model = models$design %>% dynutils::mapdf(identity)),
     metric_design
   ) %>% 
     mutate(
+      dataset_id = map_chr(model, ~.$dataset$id),
+      method_id = map_chr(model, "id"),
       script = list(script_file(str_glue("scripts/run.R"))),
       executor = list(docker_executor("komparo/tde_method_random")),
       accuracy = str_glue("{id}/{dataset_id}/{method_id}/accuracy.json") %>% purrr::map(derived_file)
@@ -24,7 +30,14 @@ get_call <- function(models) {
   
   rscript_call(
     design = design,
-    inputs = c("script", "executor", "expression", "tde_overall", "parameters"),
-    outputs = c("accuracy")
+    inputs = exprs(
+      script, 
+      executor, 
+      dataset_expression = map(model, ~.$dataset$expression),
+      dataset_tde_overall = map(model, ~.$dataset$tde_overall),
+      model_tde_overall = map(model, "tde_overall"),
+      parameters
+    ),
+    outputs = exprs(accuracy)
   )
 }
